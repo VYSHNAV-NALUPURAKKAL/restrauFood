3945cc3636fdbaaf8527c0e426e2ae1400d87770
Vyshnav Nalupurakkal <vnalupurakkal@gmail.com>
2025-05-30 00:07:51 +0530

    Shimmer UI and Search partial

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,11 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantData from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [data, setData] = useState([]);
+  const [searchData, setSearchData] = useState("");
+  console.log("body rednerd");
   useEffect(() => {
     apiFetchData();
   }, []);
@@ -13,17 +15,36 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(
-      "ullil",
-      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
     setData(
       json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
-  return (
+
+  return data.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchData}
+            onChange={(e) => {
+              setSearchData(e.target.value);
+            }}
+          ></input>
+          <button
+            className="searchBtn"
+            onClick={() => {
+              const filteredSearchData = data
+                .toLowerCase()
+                .includes(searchData.toLowerCase());
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,36 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-const Header = ()=>{
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={LOGO_URL}></img>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact us</li>
-                     <li>Cart</li>
-        
-                </ul>
-            </div>
-        </div>
-    )
-}
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+  const [btnClr, setBtnClr] = useState("lightgreen");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+          <button
+            style={{
+              backgroundColor: btnClr,
+            }}
+            className="login"
+            onClick={() => {
+              btnName === "Login"
+                ? (setBtnName("Logout"), setBtnClr("red"))
+                : (setBtnClr("lightgreen"), setBtnName("Login"));
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
